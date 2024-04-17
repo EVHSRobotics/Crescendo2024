a895792f48860248448f7aec8a40731e2b57c140
nishantj2006 <nishantj2006@gmail.com>
2024-04-16 20:29:45 -0700

    calm luh day before worlds

---
+++ src/main/java/frc/robot/autos/CheckNoteFarLeft.java
@@ -0,0 +1,22 @@
+package frc.robot.autos;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Vision;
+import frc.robot.commands.SuperStructure.IntakeMode;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+public class CheckNote extends Command{
+    
+}
